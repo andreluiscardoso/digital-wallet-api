d0a35d23c4bc186177893790051fb6b74db1f4d5
andreluiscardoso <andreluis_cardoso95@outlook.com>
2025-02-26 23:34:05 -0300

    Configurando swagger. ------------------------------
    ------------------------------
    Issue:

---
+++ src/main/java/com/andreluiscardoso/digitalwallet/infrastructure/config/OpenApiConfig.java
@@ -0,0 +1,36 @@
+package com.andreluiscardoso.digitalwallet.infrastructure.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Value("${api.swagger.security-scheme-name}")
+    private String securitySchemeName;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Digital Wallet API")
+                        .version("1.0.0")
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://opensource.org/licenses/MIT"))
+                )
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
