496e86314a2e972b993c104027aeec3527d8f369
andreluiscardoso <andreluis_cardoso95@outlook.com>
2025-02-26 23:18:13 -0300

    Criando enums RecordStatus e UserRole. ------------------------------
    ------------------------------
    Issue:

---
+++ src/main/java/com/andreluiscardoso/digitalwallet/domain/enums/RecordStatus.java
@@ -0,0 +1,10 @@
+package com.andreluiscardoso.digitalwallet.domain.enums;
+
+public enum RecordStatus {
+
+    ACTIVE,
+    INACTIVE,
+    PENDING,
+    DELETED
+
+}
---
+++ src/main/java/com/andreluiscardoso/digitalwallet/domain/enums/UserRole.java
@@ -0,0 +1,9 @@
+package com.andreluiscardoso.digitalwallet.domain.enums;
+
+public enum UserRole {
+
+    ADMIN,
+    CUSTOMER,
+    SUPPORT
+
+}
