7503ccab9f6726b13d615e690c67ce817edc7e57
andreluiscardoso <andreluis_cardoso95@outlook.com>
2025-02-26 23:21:17 -0300

    Criando classe User. ------------------------------
    ------------------------------
    Issue:

---
+++ src/main/java/com/andreluiscardoso/digitalwallet/domain/model/User.java
@@ -0,0 +1,64 @@
+package com.andreluiscardoso.digitalwallet.domain.model;
+
+import com.andreluiscardoso.digitalwallet.domain.enums.RecordStatus;
+import com.andreluiscardoso.digitalwallet.domain.enums.UserRole;
+import com.andreluiscardoso.digitalwallet.util.RecordInfo;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "\"user\"")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User implements UserDetails {
+
+    @Id
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "username", nullable = false, length = 100)
+    private String username;
+
+    @Column(name = "email", nullable = false, length = 100, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 100)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false, length = 16)
+    private UserRole role;
+
+    @Column(name = "cpf", nullable = false, length = 11, unique = true)
+    private String cpf;
+
+    @Column(name = "rg", nullable = false, length = 9, unique = true)
+    private String rg;
+
+    @Column(name = "phone", nullable = false, length = 14, unique = true)
+    private String phone;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false, length = 16)
+    private RecordStatus status;
+
+    @Embedded
+    private RecordInfo recordInfo;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+}
